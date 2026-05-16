a0c2df58a9caa4d475c5cd098375482c864c8e92
Jane Batakariza <janebatakariza@gmail.com>
2026-05-16 21:05:48 +0200

    bg successfully added and also the home page stylings are done

--- src/Components/StatsBar.tsx
+++ src/Components/StatsBar.tsx
@@ -1,11 +1,11 @@
-import './StatsBar.css';
-import React from 'react';
+import "./StatsBar.css";
+import React from "react";
 
 function StatsBar() {
   const stats = [
     { value: "50+", label: "Restaurants", gold: false },
     { value: "10k+", label: "Happy Customers", gold: false },
-    { value: "4.8⭐", label: "Average Rating", gold: false },
+    { value: "4.8", label: "Average Rating", gold: false },
     { value: "3 types", label: "Restaurant .Hotel .Pub", gold: true },
   ];
   return (
