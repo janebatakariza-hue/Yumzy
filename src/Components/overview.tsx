ffb70c3aede271d76aca2e5ed9eb032cd09ef546
Jane Batakariza <janebatakariza@gmail.com>
2026-05-18 22:03:47 +0200

    built the overview page

---
+++ src/Components/overview.tsx
@@ -0,0 +1,27 @@
+import Sidebar from "./Overview/Sidebar";
+import TopBar from "./Overview/TopBar";
+import FilterBar from "./Overview/FilterBar";
+import RestaurantCard from "./Overview/RestaurantCard";
+import Pagination from "./Overview/Pagination";
+import { restaurants } from "./Overview/restaurants.data";
+import "./overview.css";
+
+function Overview() {
+  return (
+    <div className="overview-page">
+      <Sidebar />
+      <div className="main-content">
+        <TopBar />
+        <FilterBar />
+        <div className="restaurant-grid">
+          {restaurants.map((r) => (
+            <RestaurantCard key={r.id} restaurant={r} />
+          ))}
+        </div>
+        <Pagination />
+      </div>
+    </div>
+  );
+}
+
+export default Overview;
